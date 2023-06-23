c4b23ebe4c68f70ee776e63311ee6e2beb74540c
Zainal21 <Zainalarifin080718@gmail.com>
2023-06-23 16:43:14 +0700

    init from scratch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,9 @@
-import { CacheModule, Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { DatabaseConfigService } from "@core/config/database";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useClass: DatabaseConfigService,
-    }),
-    // disabled cache redis service
-    // CacheModule.registerAsync({
-    //   useClass: CacheConfigService,
-    // }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
