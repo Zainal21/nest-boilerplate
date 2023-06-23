7bca4be91b91006603533959a206e5c96958cbb3
Zainal21 <Zainalarifin080718@gmail.com>
2023-06-23 20:00:39 +0700

    feat : add database connection and utils modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from '@core/config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/core/config/jwt/index.ts
@@ -0,0 +1,11 @@
+import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
+
+export class JwtConfigService implements JwtOptionsFactory {
+  createJwtOptions(): JwtModuleOptions | Promise<JwtModuleOptions> {
+    return {
+      signOptions: {
+        algorithm: 'HS256',
+      },
+    };
+  }
+}
