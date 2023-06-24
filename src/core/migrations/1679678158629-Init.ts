a06939016dee044f7f313e8837e2cd302e425a12
Zainal21 <Zainalarifin080718@gmail.com>
2023-06-24 09:29:41 +0700

    feat : add auth guard

--- src/core/migrations/1679678158629-Init.ts
+++ src/core/migrations/1679678158629-Init.ts
@@ -5,11 +5,11 @@ export class Init1679678158629 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "resetPasswordToken" character varying, "resetPasswordExpires" integer, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "users" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "resetPasswordToken" character varying, "resetPasswordExpires" integer, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TABLE "users"`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
+    cors: true,
+  });
+
+  // cors enable
+  app.enableCors();
+
+  // global pipes
+  app.useGlobalPipes(new ValidationPipe());
+
+  // versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
